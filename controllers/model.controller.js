3890e70a9566b6abcbf765697455618d753d7b0a
Soumyadip Mondal <mandal.soumyadip9714@gmail.com>
2021-10-11 16:16:34 +0530

    impl: create and delete model

--- controllers/model.controller.js
+++ controllers/model.controller.js
@@ -38,3 +38,64 @@ exports.update = (req, res) => {
     }
   });
 };
+
+// Adds a model to project.
+exports.create = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!",
+    });
+  }
+
+  const project_id = req.params.project_id;
+
+  project.findById(project_id, function (err, result) {
+    if (!err) {
+      if (!result) {
+        res.status(404).send("Project was not found");
+      } else {
+        result.models.push(req.body);
+        result.markModified("models");
+        result.save(function (saveerr, saveresult) {
+          if (!saveerr) {
+            res.status(200).send(saveresult);
+          } else {
+            res.status(400).send(saveerr.message);
+          }
+        });
+      }
+    } else {
+      res.status(400).send(err.message);
+    }
+  });
+};
+
+// Removes a model from a project.
+exports.remove = (req, res) => {
+  const project_id = req.params.project_id;
+  const id = req.params.id;
+
+  project.findById(project_id, function (err, result) {
+    if (!err) {
+      if (!result) {
+        res.status(404).send("Project was not found");
+      } else {
+        result.models.id(id).remove(function (removeerr, removresult) {
+          if (removeerr) {
+            res.status(400).send(removeerr.message);
+          }
+        });
+        result.markModified("models");
+        result.save(function (saveerr, saveresult) {
+          if (!saveerr) {
+            res.status(200).send(saveresult);
+          } else {
+            res.status(400).send(saveerr.message);
+          }
+        });
+      }
+    } else {
+      res.status(400).send(err.message);
+    }
+  });
+};
--- routes/models.js
+++ routes/models.js
@@ -2,7 +2,13 @@ var express = require("express");
 var router = express.Router();
 const model = require("../controllers/model.controller.js");
 
+/* POST creates a new model under a project */
+router.post("/:project_id", model.create);
+
 /* PATCH updates a model */
 router.patch("/:project_id/:id", model.update);
+
+/* PATCH updates a model */
+router.patch("/:project_id/:id/remove", model.remove);
 
 module.exports = router;
